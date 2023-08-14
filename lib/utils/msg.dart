c935c6ad571a4321c14d76aa74df195413166da3
FAISAL HALIM <ffaisalhalim@gmail.com>
2023-08-14 13:50:44 +0700

    update (snackbar) : if opened close it then open

--- lib/utils/msg.dart
+++ lib/utils/msg.dart
@@ -1,9 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:rsiap_dokter/config/colors.dart';
 import 'package:rsiap_dokter/config/config.dart';
+import 'package:rsiap_dokter/utils/fonts.dart';
+import 'package:rsiap_dokter/utils/helper.dart';
+import 'package:rsiap_dokter/utils/table.dart';
 
 class Msg {
   static show(BuildContext context, String message) {
+    if (ScaffoldMessenger.of(context).mounted) {
+      ScaffoldMessenger.of(context).hideCurrentSnackBar();
+    }
+
     ScaffoldMessenger.of(context).showSnackBar(
       SnackBar(
         content: Text(message, style: TextStyle(color: textWhite)),
@@ -14,9 +21,19 @@ class Msg {
   }
 
   static success(BuildContext context, String message) {
+    if (ScaffoldMessenger.of(context).mounted) {
+      ScaffoldMessenger.of(context).hideCurrentSnackBar();
+    }
+
     ScaffoldMessenger.of(context).showSnackBar(
       SnackBar(
-        content: Text(message, style: TextStyle(color: textWhite)),
+        content: Row(
+          children: [
+            Icon(Icons.check_circle_outline_rounded, color: textWhite),
+            const SizedBox(width: 10),
+            Text(message, style: TextStyle(color: textWhite)),
+          ],
+        ),
         duration: const Duration(seconds: snackBarDuration),
         backgroundColor: successColor,
       ),
@@ -24,9 +41,19 @@ class Msg {
   }
 
   static info(BuildContext context, String message) {
+    if (ScaffoldMessenger.of(context).mounted) {
+      ScaffoldMessenger.of(context).hideCurrentSnackBar();
+    }
+
     ScaffoldMessenger.of(context).showSnackBar(
       SnackBar(
-        content: Text(message, style: TextStyle(color: textWhite)),
+        content: Row(
+          children: [
+            Icon(Icons.info_outline_rounded, color: textWhite),
+            const SizedBox(width: 10),
+            Text(message, style: TextStyle(color: textWhite)),
+          ],
+        ),
         duration: const Duration(seconds: snackBarDuration),
         backgroundColor: infoColor,
       ),
@@ -34,9 +61,19 @@ class Msg {
   }
 
   static warning(BuildContext context, String message) {
+    if (ScaffoldMessenger.of(context).mounted) {
+      ScaffoldMessenger.of(context).hideCurrentSnackBar();
+    }
+
     ScaffoldMessenger.of(context).showSnackBar(
       SnackBar(
-        content: Text(message, style: TextStyle(color: textWhite)),
+        content: Row(
+          children: [
+            Icon(Icons.warning_outlined, color: textWhite),
+            const SizedBox(width: 10),
+            Text(message, style: TextStyle(color: textWhite)),
+          ],
+        ),
         duration: const Duration(seconds: snackBarDuration),
         backgroundColor: warningColor,
       ),
@@ -44,11 +81,84 @@ class Msg {
   }
 
   static error(BuildContext context, String message) {
+    if (ScaffoldMessenger.of(context).mounted) {
+      ScaffoldMessenger.of(context).hideCurrentSnackBar();
+    }
+
     ScaffoldMessenger.of(context).showSnackBar(
       SnackBar(
-        content: Text(message, style: TextStyle(color: textWhite)),
+        content: Row(
+          children: [
+            Icon(Icons.error_outline_rounded, color: textWhite),
+            const SizedBox(width: 10),
+            Text(message, style: TextStyle(color: textWhite)),
+          ],
+        ),
         duration: const Duration(seconds: snackBarDuration),
         backgroundColor: errorColor,
+      ),
+    );
+  }
+
+  static withData(
+    BuildContext context,
+    String category,
+    String message,
+    Icon icon,
+    Map dataForTable,
+  ) {
+    Color color;
+    switch (category) {
+      case 'success':
+        color = successColor;
+        break;
+
+      case 'info':
+        color = infoColor;
+        break;
+
+      case 'warning':
+        color = warningColor;
+        break;
+
+      case 'error':
+        color = errorColor;
+        break;
+
+      default:
+        color = primaryColor;
+    }
+
+    if (ScaffoldMessenger.of(context).mounted) {
+      ScaffoldMessenger.of(context).hideCurrentSnackBar();
+    }
+
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              children: [
+                icon,
+                const SizedBox(width: 10),
+                Text(
+                  message,
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: Helper.getFontSize(context, 18),
+                    fontWeight: fontBold,
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 8),
+            GenTable(data: dataForTable)
+          ],
+        ),
+        duration: const Duration(seconds: snackBarDuration),
+        backgroundColor: color,
       ),
     );
   }
