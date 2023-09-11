144df3d02f6a2add27a6e1a6c7f068b5e85c4c78
FAISAL HALIM <ffaisalhalim@gmail.com>
2023-09-11 09:37:05 +0700

    feat (keyboard) : hide on lost focus

--- lib/screen/login.dart
+++ lib/screen/login.dart
@@ -94,170 +94,175 @@ class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     bool isKeyboard = MediaQuery.of(context).viewInsets.bottom != 0.0;
-    return Scaffold(
-      backgroundColor: bgColor,
-      body: SafeArea(
-        child: Container(
-          alignment: Alignment.center,
-          decoration: const BoxDecoration(
-            image: DecorationImage(
-              image: AssetImage('assets/images/splash-bg.png'),
-              fit: BoxFit.cover,
-              alignment: Alignment.center,
+    return GestureDetector(
+      onTap: () {
+        FocusScope.of(context).requestFocus(FocusNode());
+      },
+      child: Scaffold(
+        backgroundColor: bgColor,
+        body: SafeArea(
+          child: Container(
+            alignment: Alignment.center,
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/images/splash-bg.png'),
+                fit: BoxFit.cover,
+                alignment: Alignment.center,
+              ),
             ),
-          ),
-          child: SingleChildScrollView(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Padding(
-                  padding:
-                      const EdgeInsets.symmetric(vertical: 30, horizontal: 20),
-                  child: Image.asset(
-                    'assets/images/logo-text-rsiap2.png',
-                    height: 150,
-                    width: 150,
+            child: SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Padding(
+                    padding:
+                        const EdgeInsets.symmetric(vertical: 30, horizontal: 20),
+                    child: Image.asset(
+                      'assets/images/logo-text-rsiap2.png',
+                      height: 150,
+                      width: 150,
+                    ),
                   ),
-                ),
-                Form(
-                  key: _formKey,
-                  child: Padding(
-                    padding: const EdgeInsets.all(20),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.stretch,
-                      children: <Widget>[
-                        Container(
-                          padding: const EdgeInsets.symmetric(
-                            horizontal: 20,
-                            vertical: 5,
-                          ),
-                          decoration: BoxDecoration(
-                            color: bgWhite,
-                            borderRadius: BorderRadius.circular(10),
-                            boxShadow: [
-                              BoxShadow(
-                                color: primaryColor.withOpacity(0.3),
-                                blurRadius: 10,
-                                offset: const Offset(0, 5),
-                              )
-                            ],
-                          ),
-                          child: TextFormField(
-                            maxLines: 1,
-                            decoration: InputDecoration(
-                              hintText: labelUsername  ,
-                              contentPadding: const EdgeInsets.all(10),
-                              border: InputBorder.none,
-                              hintStyle: TextStyle(color: textColor),
+                  Form(
+                    key: _formKey,
+                    child: Padding(
+                      padding: const EdgeInsets.all(20),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.stretch,
+                        children: <Widget>[
+                          Container(
+                            padding: const EdgeInsets.symmetric(
+                              horizontal: 20,
+                              vertical: 5,
                             ),
-                            style: TextStyle(color: textColor),
-                            onSaved: (value) {
-                              username = value!;
-                            },
-                          ),
-                        ),
-                        const SizedBox(height: 20),
-                        Container(
-                          padding: const EdgeInsets.symmetric(
-                            horizontal: 20,
-                            vertical: 5,
-                          ),
-                          decoration: BoxDecoration(
-                            color: bgWhite,
-                            borderRadius: BorderRadius.circular(10),
-                            boxShadow: [
-                              BoxShadow(
-                                color: primaryColor.withOpacity(0.3),
-                                blurRadius: 10,
-                                offset: const Offset(0, 5),
-                              )
-                            ],
-                          ),
-                          child: TextFormField(
-                            maxLines: 1,
-                            obscureText: _secureText,
-                            style: TextStyle(color: textColor),
-                            
-                            decoration: InputDecoration(
-                              hintText: labelPassword,
-                              border: InputBorder.none,
-                              contentPadding: const EdgeInsets.all(10),
-                              hintStyle: TextStyle(color: textColor),
+                            decoration: BoxDecoration(
+                              color: bgWhite,
+                              borderRadius: BorderRadius.circular(10),
+                              boxShadow: [
+                                BoxShadow(
+                                  color: primaryColor.withOpacity(0.3),
+                                  blurRadius: 10,
+                                  offset: const Offset(0, 5),
+                                )
+                              ],
                             ),
-                            onSaved: (value) {
-                              password = value!;
-                            },
+                            child: TextFormField(
+                              maxLines: 1,
+                              decoration: InputDecoration(
+                                hintText: labelUsername  ,
+                                contentPadding: const EdgeInsets.all(10),
+                                border: InputBorder.none,
+                                hintStyle: TextStyle(color: textColor),
+                              ),
+                              style: TextStyle(color: textColor),
+                              onSaved: (value) {
+                                username = value!;
+                              },
+                            ),
                           ),
-                        ),
-                        isKeyboard
-                            ? const SizedBox(height: 50)
-                            : Padding(
-                                padding: EdgeInsets.only(
-                                  top:
-                                      MediaQuery.of(context).size.height * 0.06,
-                                  bottom:
-                                      MediaQuery.of(context).size.height * 0.04,
-                                ),
-                                child: Center(
-                                  child: GestureDetector(
-                                    onTap: () {
-                                      Msg.info(context, forgotPasswordMsg);
-                                    },
-                                    child: Text(
-                                      forgotText,
-                                      style: TextStyle(color: textColor),
+                          const SizedBox(height: 20),
+                          Container(
+                            padding: const EdgeInsets.symmetric(
+                              horizontal: 20,
+                              vertical: 5,
+                            ),
+                            decoration: BoxDecoration(
+                              color: bgWhite,
+                              borderRadius: BorderRadius.circular(10),
+                              boxShadow: [
+                                BoxShadow(
+                                  color: primaryColor.withOpacity(0.3),
+                                  blurRadius: 10,
+                                  offset: const Offset(0, 5),
+                                )
+                              ],
+                            ),
+                            child: TextFormField(
+                              maxLines: 1,
+                              obscureText: _secureText,
+                              style: TextStyle(color: textColor),
+                              
+                              decoration: InputDecoration(
+                                hintText: labelPassword,
+                                border: InputBorder.none,
+                                contentPadding: const EdgeInsets.all(10),
+                                hintStyle: TextStyle(color: textColor),
+                              ),
+                              onSaved: (value) {
+                                password = value!;
+                              },
+                            ),
+                          ),
+                          isKeyboard
+                              ? const SizedBox(height: 50)
+                              : Padding(
+                                  padding: EdgeInsets.only(
+                                    top:
+                                        MediaQuery.of(context).size.height * 0.06,
+                                    bottom:
+                                        MediaQuery.of(context).size.height * 0.04,
+                                  ),
+                                  child: Center(
+                                    child: GestureDetector(
+                                      onTap: () {
+                                        Msg.info(context, forgotPasswordMsg);
+                                      },
+                                      child: Text(
+                                        forgotText,
+                                        style: TextStyle(color: textColor),
+                                      ),
                                     ),
                                   ),
                                 ),
+                          Padding(
+                            padding: const EdgeInsets.symmetric(horizontal: 50),
+                            child: ElevatedButton(
+                              onPressed: () {
+                                if (_formKey.currentState!.validate()) {
+                                  _formKey.currentState!.save();
+                                  _login();
+                                }
+                              },
+                              style: ElevatedButton.styleFrom(
+                                backgroundColor: primaryColor,
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(50),
+                                ),
+                                padding: const EdgeInsets.symmetric(
+                                  horizontal: 50,
+                                  vertical: 20,
+                                ),
                               ),
-                        Padding(
-                          padding: const EdgeInsets.symmetric(horizontal: 50),
-                          child: ElevatedButton(
-                            onPressed: () {
-                              if (_formKey.currentState!.validate()) {
-                                _formKey.currentState!.save();
-                                _login();
-                              }
-                            },
-                            style: ElevatedButton.styleFrom(
-                              backgroundColor: primaryColor,
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(50),
-                              ),
-                              padding: const EdgeInsets.symmetric(
-                                horizontal: 50,
-                                vertical: 20,
-                              ),
-                            ),
-                            child: Text(
-                              _isLoading ? processingText : loginText,
-                              style: TextStyle(
-                                color: textWhite,
-                                fontSize: 18,
-                                fontWeight: fontBold,
+                              child: Text(
+                                _isLoading ? processingText : loginText,
+                                style: TextStyle(
+                                  color: textWhite,
+                                  fontSize: 18,
+                                  fontWeight: fontBold,
+                                ),
                               ),
                             ),
                           ),
-                        ),
-                      ],
+                        ],
+                      ),
                     ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
           ),
         ),
-      ),
-      bottomNavigationBar: isKeyboard
-          ? null
-          : Padding(
-              padding: const EdgeInsets.only(bottom: 15, top: 5),
-              child: Text(credit,
-                textAlign: TextAlign.center,
-                style: TextStyle(color: textColor.withOpacity(0.6)),
+        bottomNavigationBar: isKeyboard
+            ? null
+            : Padding(
+                padding: const EdgeInsets.only(bottom: 15, top: 5),
+                child: Text(credit,
+                  textAlign: TextAlign.center,
+                  style: TextStyle(color: textColor.withOpacity(0.6)),
+                ),
               ),
-            ),
+      ),
     );
   }
 }
