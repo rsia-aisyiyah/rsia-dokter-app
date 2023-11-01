9437133f1f324850c9fe07dd271fc4933195f6b2
FAISAL HALIM <ffaisalhalim@gmail.com>
2023-11-01 07:56:55 +0700

    update (auth) : check authenticated user client

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:intl/date_symbol_data_local.dart';
+import 'package:rsiap_dokter/components/loadingku.dart';
 import 'package:rsiap_dokter/screen/menu.dart';
 import 'package:rsiap_dokter/screen/profile.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-import 'package:rsiap_dokter/config/strings.dart';
 import 'package:rsiap_dokter/screen/login.dart';
 import 'package:rsiap_dokter/screen/index.dart';
 import 'package:rsiap_dokter/config/config.dart';
@@ -31,7 +31,7 @@ class MainApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       home: Directionality(
         textDirection: TextDirection.ltr,
-        child: CheckAuth(),
+        child: SplashScreen(),
       ),
       routes: {
         '/login': (context) => const LoginScreen(),
@@ -43,55 +43,53 @@ class MainApp extends StatelessWidget {
   }
 }
 
-class CheckAuth extends StatefulWidget {
-  const CheckAuth({super.key});
-
+class SplashScreen extends StatefulWidget {
   @override
-  State<CheckAuth> createState() => _CheckAuthState();
+  _SplashScreenState createState() => _SplashScreenState();
 }
 
-class _CheckAuthState extends State<CheckAuth> {
-  bool isAuth = false;
+class _SplashScreenState extends State<SplashScreen> {
+  Future<bool> checkToken() async {
+    final prefs = await SharedPreferences.getInstance();
+    final token = prefs.getString('token');
 
-  @override
-  void initState() {
-    super.initState();
-    _checkIsLoggedin();
-  }
+    await Future.delayed(Duration(seconds: 2));
 
-  void _checkIsLoggedin() {
-    SharedPreferences.getInstance().then((prefs) {
-      var token = prefs.getString('token');
-      if (token != null) {
-        Api().postRequest('/auth/me').then((res) {
-          if (res.statusCode == 200) {
-            if (mounted) {
-              setState(() {
-                isAuth = true;
-              });
-            }
-          } else {
-            if (mounted) {
-              Msg.error(context, sessionExpiredMsg);
-              setState(() {
-                isAuth = false;
-              });
-            }
-          }
-        });
+    if (token != null) {
+      final res = await Api().postRequest('/auth/me');
+      if (res.statusCode == 200) {
+        return true; // Token valid, arahkan ke halaman indeks.
       }
-    });
+    }
+
+    return false; // Tidak ada token atau request code bukan 200, arahkan ke halaman login.
   }
 
   @override
   Widget build(BuildContext context) {
-    Widget child;
-    if (isAuth) {
-      child = const IndexScreen();
-    } else {
-      child = const LoginScreen();
-    }
-
-    return child;
+    return Scaffold(
+      body: Center(
+        child: FutureBuilder<bool>(
+          future: checkToken(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              // Tampilkan widget loading ketika sedang menunggu hasil.
+              return loadingku();
+            } else if (snapshot.hasError) {
+              // Handle error jika ada kesalahan.
+              // return Text('Error: ${snapshot.error}');
+              return Msg.error(context, 'Error: ${snapshot.error}');
+            } else {
+              // Redirect sesuai hasil pengecekan token.
+              if (snapshot.data == true) {
+                return IndexScreen();
+              } else {
+                return LoginScreen();
+              }
+            }
+          },
+        ),
+      ),
+    );
   }
 }
